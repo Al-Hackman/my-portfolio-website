85d8ce266792cc61ff296a1d2027ae12a44172e1
Albert Hackman <nana20p@yahoo.com>
2021-08-09 04:19:13 -0400

    Added Content to Main Page

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -5,9 +5,29 @@ import './MainPage.scss';
 
 function MainPage() {
     return (
-        <div>
+        <div className="main-page">
+
             <ParticlesBackground />
-            <div className='title'><span>Good Website!</span> </div> 
+
+            <section className="main-page__top">
+                {/* <span>Good Website!!!</span>  */}
+                <div className="main-page__name">ALBERT HACKMAN</div>
+                <div className="main-page__title">WEB DEVELOPER</div>
+            </section> 
+
+            <section className="main-page__middle">
+                <div className="main-page__menu">ABOUT</div>
+                <div className="main-page__divider"></div>
+                <div className="main-page__menu">PROJECTS</div>
+                <div className="main-page__divider"></div>
+                <div className="main-page__menu">SKILLS</div>
+            </section>
+
+            <section className="main-page__bottom"> 
+                <div className="main-page__contact">GITHUB</div>
+                <div className="main-page__contact">LINKEDIN</div>
+            </section>
+
         </div>
     )
 }
